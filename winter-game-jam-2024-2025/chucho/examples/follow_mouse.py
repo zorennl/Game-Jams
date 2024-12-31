1d189cb33b1ff74dcd3c254baa368f15a5703510
BoinkGit <rmelyle@gmail.com>
2024-12-31 15:26:30 -0500

    simplified code

--- winter-game-jam-2024-2025/chucho/examples/follow_mouse.py
+++ winter-game-jam-2024-2025/chucho/examples/follow_mouse.py
@@ -10,31 +10,35 @@ dist = 50
 playerx = 500
 playery = 500
 
+def normAngle(px,py):
+    mx = get_mouse_x()
+    my = get_mouse_y()
+    dx = mx-px; dy = my-py
+    if dx == 0:
+        if dy > 0:
+            dy = dist
+        else: dy = -1*dist
+    else:
+        angle = m.atan(dy/dx)
+        dx = int(m.cos(angle)*dist)
+        dy = int(m.sin(angle)*dist)
+    if mx < px:
+        dx *= -1; dy *= -1
+    return (dx,dy)
+
+
+
+
+
+
 while not window_should_close():
     begin_drawing()
     clear_background(WHITE)
 
-    dx = get_mouse_x()-playerx
-    dy = get_mouse_y()-playery
-    draw_text(f"dx: {dx}, dy: {dy}",0,0,20,DARKPURPLE)
-    if dx == 0:
-        dy = -1*dist
-    else: 
-        angle = dy/dx
-        dx = int(m.cos(m.atan(angle))*dist)
-        dy = int(m.sin(m.atan(angle))*dist)
-    draw_text(f"angle (rad): {m.atan(angle)}",0,30,20,DARKPURPLE)
-    draw_text(f"angle (deg): {m.degrees(m.atan(angle))}",0,60,20,DARKPURPLE)
-    if get_mouse_x() < playerx:
-        dx *= -1
-        dy *= -1
-
-    if get_mouse_x()-playerx == 0:
-        if get_mouse_y()-playery > 0:
-            dy = dist
-
     draw_circle(playerx,playery,10,RED)
-    draw_rectangle(playerx+dx,playery+dy,10,10,ORANGE)
+    swordx = normAngle(playerx,playery)[0]
+    swordy = normAngle(playerx,playery)[1]
+    draw_rectangle(playerx+swordx,playery+swordy,10,10,ORANGE)
 
 
     end_drawing()
