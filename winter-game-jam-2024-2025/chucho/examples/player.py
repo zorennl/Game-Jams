b291ce6abf39fcf3e798613551ed1c914835b597
BoinkGit <rmelyle@gmail.com>
2025-01-03 00:39:12 -0500

    bug fixing player/texture

--- winter-game-jam-2024-2025/chucho/examples/player.py
+++ winter-game-jam-2024-2025/chucho/examples/player.py
@@ -1,4 +1,6 @@
 from pyray import *
+from os.path import join as os
+
 
 # CONSTANTS
 WINDOW_WIDTH = 800
@@ -6,21 +8,23 @@ WINDOW_HEIGHT = 500
 
 # PLAYER CLASS
 class player:
-    def __init__(self, pos, spd, size, rotation, texture):
-        self.pos = Vector2()
+    def __init__(self, pos: Vector2, spd, size: float, rotation, texture):
+        self.pos = pos
         self.spd = spd
-        self.size = float()
+        self.size = size
         self.rotation = rotation
         self.texture = texture
+
+    def draw(self):
         # self.tint = Color()
-        # load_texture(texture)
-        draw_texture_ex(texture, pos, rotation, size, WHITE)
+        # load_texture(sef.texture)
+        draw_texture_ex(self.texture, self.pos, self.rotation, self.size, WHITE)
         
-    def move(self, up, down, left, right):
-        self.up = KeyboardKey
-        self.down = KeyboardKey
-        self.left = KeyboardKey
-        self.right = KeyboardKey
+    def move(self, up: KeyboardKey, down: KeyboardKey, left: KeyboardKey, right: KeyboardKey):
+        self.up = up
+        self.down = down
+        self.left = left
+        self.right = right
         if is_key_down(up):
             self.pos.y -= self.spd
         if is_key_down(down):
@@ -30,21 +34,27 @@ class player:
         if is_key_down(right):
             self.pos.x += self.spd
 
-playertextureraw = 'assets/player.png'
-playertexture = load_texture(playertextureraw)
+
 
 testpos = Vector2(400,250)
 
 init_window(WINDOW_WIDTH,WINDOW_HEIGHT,"player")
 
+playertextureraw = os('assets','player.png')
+playertexture = load_texture(playertextureraw)
+
 set_target_fps(60)
 
 set_exit_key(KEY_Q)
 
+test = player(testpos,2,2,0,playertexture)
+
 while not window_should_close():
     begin_drawing()
+    clear_background(BLACK)
 
-    test = player(testpos,2,2,0,playertexture)#.move(KEY_W,KEY_S,KEY_A,KEY_D)
+    test.draw()
+    test.move(KEY_W,KEY_S,KEY_A,KEY_D)
     
     end_drawing()
 
