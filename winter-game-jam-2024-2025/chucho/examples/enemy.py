5f16ea9ee421817d4623a3b4f99e7ed5a8bb7048
BoinkGit <rmelyle@gmail.com>
2025-01-02 22:58:02 -0500

    second type, ready to import

--- winter-game-jam-2024-2025/chucho/examples/enemy.py
+++ winter-game-jam-2024-2025/chucho/examples/enemy.py
@@ -1,7 +1,6 @@
 from pyray import *
 import math as m
 
-
 class enemy:
     def __init__(self,x,y,spd,type):
         self.x = x
@@ -12,6 +11,19 @@ class enemy:
         if self.type == 1:
             self.x += normAngle(self.x,self.y,get_mouse_x(),get_mouse_y(),self.spd)[0]
             self.y += normAngle(self.x,self.y,get_mouse_x(),get_mouse_y(),self.spd)[1]
+        if self.type == 2:
+            try: self.vx += 0
+            except: self.vx = 0
+
+            try: self.vy += 0
+            except: self.vy = 0
+
+            randspd = get_random_value(1,10)*.1
+            self.vx += normAngle(self.x,self.y,get_mouse_x(),get_mouse_y(),self.spd*randspd)[0]
+            self.vy += normAngle(self.x,self.y,get_mouse_x(),get_mouse_y(),self.spd*randspd)[1]
+            self.vx *= .8; self.vy *= .8
+            self.x += self.vx; self.y += self.vy
+
 
 
 def normAngle(px,py,twx,twy,dist):
@@ -22,8 +34,8 @@ def normAngle(px,py,twx,twy,dist):
         else: dy = -1*dist
     else:
         angle = m.atan(dy/dx)
-        dx = int(m.cos(angle)*dist)
-        dy = int(m.sin(angle)*dist)
+        dx = m.cos(angle)*dist
+        dy = m.sin(angle)*dist
     if twx < px:
         dx *= -1; dy *= -1
     return dx, dy
@@ -41,17 +53,15 @@ while not window_should_close():
     begin_drawing()
     clear_background(WHITE)
 
-    if is_key_pressed(KEY_A):
-        enemyList.append(enemy(0,0,50,1))
-    if is_key_pressed(KEY_S):
-        enemyList.append(enemy(0,0,25,1))
-    if is_key_pressed(KEY_D):
+    if is_key_pressed(KEY_ONE):
         enemyList.append(enemy(0,0,2,1))
-    
+    if is_key_pressed(KEY_TWO):
+        enemyList.append(enemy(0,0,2,2))
+
     i = 0
     for item in enemyList:
 
-        draw_rectangle(item.x,item.y,20,20,RED)
+        draw_rectangle(int(item.x),int(item.y),20,20,RED)
         item.move()
         if abs(item.x-get_mouse_x()) < 10:
             if abs(item.y-get_mouse_y()) < 10:
